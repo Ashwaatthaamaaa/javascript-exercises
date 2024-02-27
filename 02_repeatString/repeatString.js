187ad07d69d367f79ef577dedf0e10e8053c188c
Ashwaatthaamaaa <sarthakk145@gmail.com>
2024-02-27 18:33:05 +0530

    complete js file for 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string,time) {
+    let repeatedString = "";
+    if (time<0) return "ERROR" 
+    while(time>0){
+        repeatedString = repeatedString + string;
+        time--;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
