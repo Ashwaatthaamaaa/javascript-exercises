a721453b7f1fd21b2ea4a0663c384512e0bb73a9
Ashwaatthaamaaa <sarthakk145@gmail.com>
2024-02-27 23:37:54 +0530

    complete js file for 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    if(!(Number.isInteger(a) && Number.isInteger(b)) || (a<0 || b<0)){return "ERROR"}
+    let num1=a;
+    let num2=b;
+    if(a>b)
+    {
+        num2=a;
+        num1=b;        
+    }
+    let sum = 0;
+    for(let i=num1;i<=num2;i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
