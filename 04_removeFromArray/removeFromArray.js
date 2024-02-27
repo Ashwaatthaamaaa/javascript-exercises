679f21fe2cc14440ce4a532e5dc785885da0b0b4
Ashwaatthaamaaa <sarthakk145@gmail.com>
2024-02-27 21:00:13 +0530

    complete js file for 0

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...theArgs) {
+    let workArr=[];
+    function remove(array,element){
+        let workArr=[];
+            for(let i = 0; i < array.length ; i++)
+                if (!(array[i] === element)) { 
+                     workArr.push(array[i]);
+                    //console.log("Removed element: " + spliced); 
+                    //console.log("Remaining elements: " + arr); 
+                } 
+        return workArr;
+    }
+    
+    for(let i=0;i<theArgs.length;i++)
+    {
+        workArr = remove(array,theArgs[i]);
+        array=workArr;
+    }
+    
+    return workArr;
+
+
+
 
 };
 
