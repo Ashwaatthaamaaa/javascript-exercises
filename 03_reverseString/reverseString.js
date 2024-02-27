90cb963d397972c07f5b39086bf6d1b060589c67
Ashwaatthaamaaa <sarthakk145@gmail.com>
2024-02-27 18:45:55 +0530

    complete js file for 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let array = string.split("");
+    let reversedString = "";
+    let i = string.length-1;
+    while(i >= 0){
+        reversedString = reversedString + array[i];
+        i--;
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
