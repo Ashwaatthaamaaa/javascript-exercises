3e9fb9986e5f610c4f67ca717c3fed91a305bbf6
Ashwaatthaamaaa <sarthakk145@gmail.com>
2024-03-05 02:26:53 +0530

    pass the test-case for 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let arrayString = string.toLowerCase().split('');
+    arrayString = arrayString.filter((char) => /^[a-zA-Z0-9]+$/.test(char) );
+    let reverseString = arrayString.slice().reverse();
+    return     arrayString.join("") === reverseString.join("");
 
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
