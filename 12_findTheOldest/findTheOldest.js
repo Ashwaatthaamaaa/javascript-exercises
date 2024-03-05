edfba1f14d32023bfa34e4bcf9fad169d1e28648
Ashwaatthaamaaa <sarthakk145@gmail.com>
2024-03-05 16:40:54 +0530

    pass the test-case for 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let age = people.map((individual) => {
+        if (individual.yearOfDeath) {
+            return [ individual.name, individual.yearOfDeath - individual.yearOfBirth];
+        } else {
+            return [ individual.name, 2024 - individual.yearOfBirth ];
+        }
+    });
+    let sortAge = age.sort((a,b) => b[1] - a[1]);
+    return people.find((individual) => individual['name'] == sortAge[0][0]);
+    
+    
 };
 
 // Do not edit below this line
